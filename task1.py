363b8173c859c94f626b800cdcf080531ece5c4b
Azarov Oleg <74871854+deels@users.noreply.github.com>
2021-06-04 22:49:21 +0300

    Add files via upload

---
+++ task1.py
@@ -0,0 +1,16 @@
+# TODO:Task 1
+#  Write a function called oops that explicitly raises an IndexError exception when called. Then write another
+#  function that calls oops inside a try/except state­ment to catch the error. What happens if you change oops to
+#  raise KeyError instead of IndexError?
+
+def oops():
+    raise IndexError
+    return
+
+def second_oops():
+    try:
+        oops()
+    except IndexError:
+        print('Ошибка обнаружена.')
+
+second_oops()
